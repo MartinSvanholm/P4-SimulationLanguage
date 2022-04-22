73161e97e4403692e00f8a4875bc24f773f61662
MartinSvanholm <msvanh20@student.aau.dk>
2022-04-22 10:18:16 +0200

    Delete unused files

--- src/Models/Node.java
+++
@@ -1,8 +0,0 @@
-package Models;
-
-import java.awt.*;
-import java.util.ArrayList;
-
-public class Node {
-    ArrayList<Node> Connection = new ArrayList<>();
-}
--- src/Models/Road.java
+++
@@ -1,8 +0,0 @@
-package Models;
-
-public class Road {
-    float length;
-    float speedLimit;
-    Node startNode = new Node();
-    Node endNode = new Node();
-}
