20405c8829cf5b7ac923c8922632f90b67cbc0b7
MartinSvanholm <msvanh20@student.aau.dk>
2022-05-17 13:32:59 +0200

    added vehicle update feature

--- src/VisitorHelpers/TypeCheckHelper.java
+++ src/VisitorHelpers/TypeCheckHelper.java
@@ -2,6 +2,7 @@ package VisitorHelpers;
 
 import ASTNodes.ExprNodes.FunctionCallNode;
 import ASTNodes.Identifier.SimpleIdNode;
+import ASTNodes.Node;
 import SymbolTable.Symbol;
 import SymbolTable.SymbolTable;
 import SymbolTable.GlobalSymbolTable;
@@ -41,5 +42,46 @@ public class TypeCheckHelper extends BaseHelper{
                 return table.Type;
         }
         return typeError + "." + identifier;
+    }
+
+    public String TypeCheckAgent(String funcName, Node node) {
+        SymbolTable func = FindVehicleTable(GlobalSymbolTable, funcName, 0);
+        if(func == null) {
+            AddError(node, "Vehicle does not contain " + funcName);
+            return typeError + "." + funcName;
+        }
+        return func.Type;
+    }
+
+    private SymbolTable FindVehicleTable(SymbolTable symbolTable, String funcName, int i) {
+        for(SymbolTable table : symbolTable.Children) {
+
+            if(table.Type.strip().equals("Vehicle")) {
+                for(SymbolTable func : table.Children) {
+                    if(func.Name.equals(funcName))
+                        return func;
+                }
+            }
+
+            if(!table.Children.isEmpty()) {
+                SymbolTable var = FindVehicleTable(table, funcName, 0);
+                if(var != null)
+                    return var;
+            }
+        }
+
+        if(i == 0) {
+            for(SymbolTable table : GlobalSymbolTable.PredifindValues) {
+                if(!table.Children.isEmpty()) {
+                    SymbolTable var = FindTableByName(table, funcName, i + 1);
+                    if(var != null)
+                        return var;
+                }
+                if(table.Name.equals(funcName))
+                    return table;
+            }
+        }
+
+        return null;
     }
 }
