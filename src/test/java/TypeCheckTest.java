f50e1f5557a8b7fbc95c515567008b2af908bace
Julian <julian.larsen07@gmail.com>
2022-05-19 15:13:11 +0200

    Burde fixe det.... Når den bliver lavet færdig

--- src/test/java/TypeCheckTest.java
+++ src/test/java/TypeCheckTest.java
@@ -14,7 +14,7 @@ import SymbolTable.GlobalSymbolTable;
 import SymbolTable.Symbol;
 import SymbolTable.SymbolTable;
 import Visitors.TypeChecker;
-import ErrorHandler;
+import Main.ErrorHandler;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
