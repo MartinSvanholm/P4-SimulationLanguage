37e1d23ef5632c9c81c5cc1c3913285bf6c7bb5d
Julian <julian.larsen07@gmail.com>
2022-05-23 11:00:33 +0200

    weird stuff using spring??

--- src/test/java/it/TypeCheckIT.java
+++ src/test/java/it/TypeCheckIT.java
@@ -1,27 +1,37 @@
 package it;
 
+import ASTNodes.ControlStructures.SwitchBody;
+import ASTNodes.ControlStructures.SwitchNode;
+import ASTNodes.ControlStructures.WhileLoopNode;
 import ASTNodes.ValueNodes.BoolNode;
 import ASTNodes.ValueNodes.NumberNode;
+import ASTNodes.ValueNodes.StringNode;
 import Main.ErrorHandler;
 import SymbolTable.GlobalSymbolTable;
 import SymbolTable.SymbolTable;
 import Visitors.TypeChecker;
 import org.junit.Test;
-import org.junit.experimental.categories.Category;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 interface IntegrationTest{}
 
-@Category(IntegrationTest.class)
+@RunWith(MockitoJUnitRunner.class)
 public class TypeCheckIT {
 
     GlobalSymbolTable globalSymbolTable;
     TypeChecker tc;
     BoolNode boolNode;
     NumberNode numberNode;
+    @Mock
+    WhileLoopNode whileMock;
 
     @BeforeEach
     void setUp() {
@@ -40,14 +50,26 @@ public class TypeCheckIT {
 
         boolNode = new BoolNode();
         numberNode = new NumberNode();
+        MockitoAnnotations.initMocks(this);
     }
 
     @Test
-    @DisplayName("Object IT")
-    public void objectCheck(){
+    @DisplayName("While IT")
+    public void whileCheck(){
 
-        
-        assertEquals("Test1Success", "Test1Success");
+        Mockito.when(whileMock.).thenReturn();
+        Double whileLoop = this.calculateArea.calculateArea(Type.RECTANGLE, 5.0d, 4.0d);
+
+
+        SwitchNode switchMock = new SwitchNode();
+        switchMock.Body = new SwitchBody();
+        switchMock.Body.cases.add(new NumberNode());
+        switchMock.Body.cases.add(new NumberNode());
+        switchMock.switchValue = new StringNode();
+
+        assertEquals("Test1Success", tc.visitSwitchNode(switchMock));
+
+        //assertEquals("Test1Success", "Test1Success");
     }
 
 
